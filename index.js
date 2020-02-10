3978879bdb7eae8be08ef578a0100195b7607e3d
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-10 15:37:27 -0500

    specifications completed for GET users and POST user

--- index.js
+++ index.js
@@ -8,6 +8,9 @@
  server.use(express.json());
 
 
+
+
+
 // initial get when running the server
 server.get('/', (req, res) => {
   res.json({ server: "Running"})
@@ -21,38 +24,44 @@ server.get('/api/users', (req, res) => {
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ error: 'Servers broken, yo.'})
+      res.status(500).json({ errorMessage: "The users information could not be retrieved." })
     })
 })
 // postman successfully responded with the data of our users
+// 1.) Specifications only call for an error for the server, which there is
 
 // get a user by id -> utilize req.params.id to get the id of the user
 server.get('/api/users/:id', (req, res) => {
   Users.findById(req.params.id)
     .then(user => {
-      res.status(200).json(user);
+      res.status(200).json(user)
     })
     .catch(err => {
       console.log(err);
-      res.status(500).json({error: "Servers broke, yo."});
+      res.status(500).json({ errorMessage: "The user information could not be retrieved." });
     })
 })
 // postman successfully returns the user specifeid by the id
 
 // add a new user -> utilize req.body
 server.post('/api/users', (req, res) => {
-  const userInfo = req.body
-  
-  Users.insert(userInfo)
-    .then(user => {
-      res.status(201).json(user)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({ error: 'Servers broken, yo.'})
-    })
+  const userInfo = req.body;
+
+  req.body.name && req.body.bio
+  ?
+    Users.insert(userInfo)
+      .then(user => {
+        res.status(201).json(user)
+      })
+      .catch(err => {
+        console.log(err)
+        res.status(500).json({ errorMessage: "There was an error while saving the user to the database" })
+      })
+  :
+      res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
 })
-// postman successfully added a new user to our users
+// 1.) postman successfully added a new user to our users
+// 2.) postman returns the correct errors based on what the request receives
 
 // delete a user -> utilize req.params.id
 server.delete('/api/users/:id', (req, res) => {
@@ -83,6 +92,10 @@ server.put('/api/users/:id', (req, res) => {
     })
 })
 // postman successfully updated the desired user
+
+
+
+
 
  const port = 5000; // declare our port
 
