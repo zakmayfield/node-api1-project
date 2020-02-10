b8dac40646e54c7c51b67344b9af2423a8c08f9c
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-10 14:49:58 -0500

    implemented a GET and POST to see users and add a user

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 // implement your API here
+ const express = require('express'); // import express
+
+ const server = express() // declare our server
+
+ const Users = require('./data/db.js')
+
+ server.use(express.json());
+
+
+// initial get when running the server
+server.get('/', (req, res) => {
+  res.json({ server: "Running"})
+})
+
+// test GET
+server.get('/names', (req, res) => {
+  const names = [
+    {
+      name: 'Joe'
+    },
+    {
+      name: 'Ellie'
+    }
+  ]
+  res.status(200).json(names)
+})
+
+// get users
+server.get('/api/users', (req, res) => {
+  Users.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'Servers broken, yo.'})
+    })
+})
+// postman successfully responded with the data of our users
+
+// add a new user -> utilize req.body
+server.post('/api/users', (req, res) => {
+  const userInfo = req.body
+  
+  Users.insert(userInfo)
+    .then(user => {
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: 'Servers broken, yo.'})
+    })
+})
+//postman successfully added a new user to our users
+
+
+ const port = 5000; // declare our port
+
+ server.listen(port, () => console.log(`\n API listening on port ${port} \n`)); // tell our server to listen on our port
